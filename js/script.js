5751e2bb89e1ef7bf4e1d1c3521384990635adb5
Vansh yadav <Vanshyadav7452@gmail.com>
2026-05-01 20:56:29 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+// EMAILJS INIT
+emailjs.init("nTvZb8PbMDrqjcNWP");
+
+// PRICE DATA
+const prices = {
+  "Kia Sonet": 3500,
+  "Honda Amaze": 3200,
+  "Maruti Brezza": 3200,
+  "Ertiga": 3500,
+  
+  "Swift": 3200,
+  "Thar": 2500,
+  "Activa": 500
+};
+
+// TOAST
+function showToast(msg){
+  const toast = document.getElementById("toast");
+  toast.innerText = msg;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
+}
+
+// PRICE CALCULATOR
+document.getElementById("checkPrice").addEventListener("click", function(){
+
+  const vehicle = document.getElementById("vehicle").value;
+  const days = document.getElementById("days").value;
+
+  if(vehicle === "Select Vehicle" || !days){
+    showToast("⚠️ Fill all details");
+    return;
+  }
+
+  const total = prices[vehicle] * days;
+
+  document.getElementById("priceBox").innerText =
+    "Estimated Price: ₹" + total;
+
+});
+
+// AUTO FILL VEHICLE
+document.querySelectorAll(".rent-btn").forEach(btn => {
+  btn.addEventListener("click", function(){
+
+    const vehicle = this.parentElement.querySelector("h3").innerText;
+
+    document.getElementById("vehicle").value = vehicle;
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+  });
+});
+
+// FORM SUBMIT
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  emailjs.sendForm("service_kqf2bur", "template_vteon2w", form)
+    .then(() => {
+      showToast("✅ Booking sent!");
+      form.reset();
+    })
+    .catch(() => {
+      showToast("❌ Failed!");
+    });
+});
+
+// WHATSAPP
+document.getElementById("waBtn").addEventListener("click", function(){
+
+  const vehicle = document.getElementById("vehicle").value;
+  const days = document.getElementById("days").value;
+
+  const msg = encodeURIComponent(
+    `Rental Request 🚖
+Vehicle: ${vehicle}
+Days: ${days}`
+  );
+
+  this.href = `https://wa.me/918126470424?text=${msg}`;
+});
